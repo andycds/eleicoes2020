480b0b1266238400b039efb761435d7266a0dd98
Anderson Sanches <andycds@gmail.com>
2020-06-11 21:32:37 -0300

    Start Spring Boot

---
+++ src/main/java/br/pro/software/eleicoes2020/Eleicoes2020Application.java
@@ -0,0 +1,13 @@
+package br.pro.software.eleicoes2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eleicoes2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eleicoes2020Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/pro/software/eleicoes2020/Eleicoes2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pro.software.eleicoes2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eleicoes2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
