d160b3393c3286cc52299c9349520265e794062d
Anderson Sanches <andycds@gmail.com>
2020-06-21 01:50:59 -0300

    Procfile prod

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-insert into eleicao (id, nome, cabecalho, rodape, inicio, fim, senha) values (1, 'Eleição Teste 1', 'Eleição 1', 'Obrigado por votar', CURRENT_TIMESTAMP(), '2021-06-11T23:30', 'master');
+insert into eleicao (id, nome, cabecalho, rodape, inicio, fim, senha) values (1, 'Eleição Teste 1', 'Eleição 1', 'Obrigado por votar', now(), '2021-06-11T23:30', 'master');
 insert into candidato (id, nome, eleicao_id) values (1, 'Anderson', 1);
 insert into candidato (id, nome, eleicao_id) values (2, 'Lili', 1);
 insert into pessoa (id, nome, documento, senha, email, apto, eleicao_id) values (1, 'admin', 'admin', 'admin', 'andycds@gmail.com', true, 1);
